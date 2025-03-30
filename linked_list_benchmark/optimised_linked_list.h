7e216b257c2125d7c1ff82c51f50abf101ccaa5f
JoshuaGregory2 <joshua.gregory@naturealpha.ai>
2025-03-30 18:12:28 +0100

    sdgsdf

--- linked_list_benchmark/optimised_linked_list.h
+++ linked_list_benchmark/optimised_linked_list.h
@@ -10,7 +10,7 @@ typedef struct OptimisedNode {
     struct OptimisedNode* next;
     struct OptimisedNode* prev;  // Added for doubly linked list support.
     struct OptimisedNode* next_free;
-} OptimisedNode __attribute__((aligned(CACHE_LINE_SIZE)));
+} OptimisedNode __attribute__();
 
 typedef struct OptimisedChunk {
     OptimisedNode* chunk;
