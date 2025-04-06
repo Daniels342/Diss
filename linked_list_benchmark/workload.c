a43089af4acbc9a7ecba38d0151345fad37821e4
JoshuaGregory2 <joshua.gregory@naturealpha.ai>
2025-04-06 19:13:03 +0100

    main

--- linked_list_benchmark/workload.c
+++ linked_list_benchmark/workload.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "workload.h"
+#include "workload.h"  // Assuming this header defines Node and your list functions
 
 int random_in_range(int min, int max) {
     return rand() % (max - min + 1) + min;
@@ -10,41 +10,52 @@ int random_in_range(int min, int max) {
 void run_workload(Node** head, int insert_percentage, int search_percentage, int delete_percentage, int duration_seconds) {
     int total_operations = 0;
     int insert_count = 0, search_count = 0, delete_count = 0;
-    clock_t insert_time = 0;
-    clock_t search_time = 0;
-    clock_t delete_time = 0;
+    double insert_time = 0.0;
+    double search_time = 0.0;
+    double delete_time = 0.0;
 
-    time_t start_time = time(NULL);
-    time_t current_time;
-    
-    while ((current_time = time(NULL)) - start_time < duration_seconds) {
+    // Use clock_gettime to get high-resolution start time
+    struct timespec start_time, current_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+    double elapsed_sec = 0.0;
+
+    while (elapsed_sec < duration_seconds) {
         int operation_choice = rand() % 100;
         int random_value = random_in_range(1, 10000);
-        
+        struct timespec op_start, op_end;
+        double diff = 0.0;
+
         if (operation_choice < insert_percentage) {
-            clock_t start = clock();
+            clock_gettime(CLOCK_MONOTONIC, &op_start);
             list_insert(head, random_value);
-            clock_t end = clock();
-            insert_time += end - start;
+            clock_gettime(CLOCK_MONOTONIC, &op_end);
+            diff = (op_end.tv_sec - op_start.tv_sec) + (op_end.tv_nsec - op_start.tv_nsec) / 1e9;
+            insert_time += diff;
             insert_count++;
         } else if (operation_choice < insert_percentage + search_percentage) {
-            clock_t start = clock();
+            clock_gettime(CLOCK_MONOTONIC, &op_start);
             list_search(*head, random_value);
-            clock_t end = clock();
-            search_time += end - start;
+            clock_gettime(CLOCK_MONOTONIC, &op_end);
+            diff = (op_end.tv_sec - op_start.tv_sec) + (op_end.tv_nsec - op_start.tv_nsec) / 1e9;
+            search_time += diff;
             search_count++;
         } else if (operation_choice < insert_percentage + search_percentage + delete_percentage) {
-            clock_t start = clock();
+            clock_gettime(CLOCK_MONOTONIC, &op_start);
             list_delete(head, random_value);
-            clock_t end = clock();
-            delete_time += end - start;
+            clock_gettime(CLOCK_MONOTONIC, &op_end);
+            diff = (op_end.tv_sec - op_start.tv_sec) + (op_end.tv_nsec - op_start.tv_nsec) / 1e9;
+            delete_time += diff;
             delete_count++;
         }
         total_operations++;
+
+        // Update elapsed time using high-resolution clock
+        clock_gettime(CLOCK_MONOTONIC, &current_time);
+        elapsed_sec = (current_time.tv_sec - start_time.tv_sec) + (current_time.tv_nsec - start_time.tv_nsec) / 1e9;
     }
 
     printf("Total Operations: %d\n", total_operations);
-    printf("Insertions: %d, Time spent: %.4f seconds\n", insert_count, (double)insert_time / CLOCKS_PER_SEC);
-    printf("Searches: %d, Time spent: %.4f seconds\n", search_count, (double)search_time / CLOCKS_PER_SEC);
-    printf("Deletions: %d, Time spent: %.4f seconds\n", delete_count, (double)delete_time / CLOCKS_PER_SEC);
+    printf("Insertions: %d, Time spent: %.4f seconds\n", insert_count, insert_time);
+    printf("Searches: %d, Time spent: %.4f seconds\n", search_count, search_time);
+    printf("Deletions: %d, Time spent: %.4f seconds\n", delete_count, delete_time);
 }
