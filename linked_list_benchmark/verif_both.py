ddb627a293946c0f59e0854319acd399d8877534
JoshuaGregory2 <joshua.gregory@naturealpha.ai>
2025-04-09 03:01:12 +0100

    drfgdfg

--- linked_list_benchmark/verif_both.py
+++ linked_list_benchmark/verif_both.py
@@ -93,7 +93,6 @@ static inline int check_list_length(u64 head_addr) {
     if (prev && (now - *prev < TWO_SECONDS)) {
         return 0; // Throttled: less than 2 seconds since last check.
     }
-            bpf_trace_printk("Doing length check on head_addr: 0x%lx\\n", head_addr);
     int count = 0;
     u64 curr = 0;
     bpf_probe_read_user(&curr, sizeof(curr), (void *)head_addr);
@@ -109,6 +108,7 @@ static inline int check_list_length(u64 head_addr) {
         curr = next;
     }
     int *exp = expected_len.lookup(&key);
+    bpf_trace_printk("List length: %d\\n", count);
     if (exp && count != *exp) {
         bpf_trace_printk("ERROR: Linked list length mismatch! Expected %d, Found %d\\n", *exp, count);
     }
